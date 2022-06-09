3de7c83ae050888d1f3ae04d11b1bbe17b4f2778
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2022-06-09 15:38:20 +0800

    about jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import '../about/about.css'
+import ME from '../../assets/me-about.jpg'
+import {FiAward} from 'react-icons/fi'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className='about_content'>
+          <div className="about_cards">
+            <article className="about_card">
+              <FiAward className='about_icon'/>
+              <h5>Experience</h5>
+              <small>2+ Years Working</small>
+            </article>
+            <article className="about_card">
+              <FiUsers className='about_icon'/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about_card">
+              <VscFolderLibrary className='about_icon'/>
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
