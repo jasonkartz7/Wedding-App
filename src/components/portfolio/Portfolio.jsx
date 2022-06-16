120bc51d574b67892fd451262a5279eb330cd053
jasonkartz7 <69835301+jasonkartz7@users.noreply.github.com>
2022-06-16 15:52:14 +0800

    portfolio css

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -13,54 +13,66 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className="container porfolio_container">
+      <div className="container portfolio_container">
         <article className='portfolio_item'>
           <div className="portfolio_item-image">
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/jasonkartz" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribbble.com/jasonkartz" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
         <article className='portfolio_item'>
           <div className="portfolio_item-image">
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/jasonkartz" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribbble.com/jasonkartz" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
         <article className='portfolio_item'>
           <div className="portfolio_item-image">
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/jasonkartz" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribbble.com/jasonkartz" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
         <article className='portfolio_item'>
           <div className="portfolio_item-image">
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/jasonkartz" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribbble.com/jasonkartz" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
         <article className='portfolio_item'>
           <div className="portfolio_item-image">
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/jasonkartz" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribbble.com/jasonkartz" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
         <article className='portfolio_item'>
           <div className="portfolio_item-image">
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/jasonkartz" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className="portfolio_item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribbble.com/jasonkartz" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
       </div>
     </section>
